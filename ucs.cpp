a10aee7a3ecfd1663448dd67e278512e67546177
Mohammad_Sazzad <121119379+MohammadSazzad@users.noreply.github.com>
2024-03-04 23:43:46 +0600

    Update ucs.cpp

--- ucs.cpp
+++ ucs.cpp
@@ -1,42 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define int long long int
 using namespace std;
 
-const int N=100005;
-// Array to store minimum distance to each node
-vector<vector<pair<int, int>>> g(N);
-
 // Function to perform Uniform Cost Search
-void UCS(int start, int end)
+void UCS(vector<vector<pair<int, int>>> &graph, int start, int end)
 {
     // Priority queue to store nodes based on their cost
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
-    vector<int> d(g.size(),INT_MIN);
-    // Array to track visited nodes 
-    vector<bool> vis(g.size(),false); 
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    // Array to store minimum distance to each node
+    vector<int> distance(graph.size(), INT_MAX);
+    // Array to track visited nodes
+    vector<bool> visited(graph.size(), false);
     pq.push({0, start});
-    d[start] = 0;
-    while (!pq.empty()) 
+    distance[start] = 0;
+
+    while (!pq.empty())
     {
         int u = pq.top().second;
         pq.pop();
-        if (vis[u]) continue;
-        vis[u] = true;
+        if (visited[u])
+            continue;
+        visited[u] = true;
         // If the current node is the goal node, we can terminate
-        if (u == end) {
-            cout << "Shortest distance from " << start << " to " << end << " is: " << d[u] << endl;
+        if (u == end)
+        {
+            cout << "Shortest distance from " << start << " to " << end << " is: " << distance[u] << endl;
             return;
         }
         // Iterate through neighbors of the current node
-        for (auto e : g[u]) 
+        for (const auto &edge : graph[u])
         {
-            int v = e.first;
-            int w = e.second;
+            int v = edge.first;
+            int weight = edge.second;
+
             // Relaxation step: update the distance if a shorter path is found
-            if (d[u] + w < d[v]) 
+            if (distance[u] + weight < distance[v])
             {
-                d[v] = d[u] + w;
-                pq.push({d[v], v});
+                distance[v] = distance[u] + weight;
+                pq.push({distance[v], v});
             }
         }
     }
@@ -44,31 +45,31 @@ void UCS(int start, int end)
     cout << "No path exists from " << start << " to " << end << endl;
 }
 
-int32_t main() 
+int32_t main()
 {
-    // n number of nodes and m number of edges 
-    int n, m; 
-    cin >> n >> m;
-    g.resize(n);
+    // cout << "Enter the number of nodes and edges: ";
+    int numNodes, numEdges;
+    cin >> numNodes >> numEdges;
+    vector<vector<pair<int, int>>> graph(numNodes);
     //"Enter the edges (source destination weight);
-    for (int i = 0; i < n; ++i) {
-        int source, dest, w;
-        cin >> source >> dest >> w;
-        g[source].push_back({dest, w});
+    for (int i = 0; i < numEdges; ++i)
+    {
+        int source, destination, weight;
+        cin >> source >> destination >> weight;
+        graph[source].push_back({destination, weight});
     }
-
-
-    //Enter the start and end node
-    int start, end; cin >> start >> end;
-    UCS(start, end);
+    // Enter the start and end node
+    int start, end;
+    cin >> start >> end;
+    UCS(graph, start, end);
 
     return 0;
 }
 
-
 /*
 
 Input :
+
 6 10
 0 1 2
 0 3 5
@@ -82,6 +83,6 @@ Input :
 5 2 5
 0 5
 
-No path exists from 4 to 2
+Shortest distance from 0 to 5 is: 3
 
 */
